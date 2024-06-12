cc4dc9f753c0135a8dcb2e51609a5c1d73cba307
pgm-bavobeau <bavo.beaumon@student.arteveldehs.be>
2024-06-12 15:42:59 +0200

    update build file

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
  
 # Install PHP & WGET
-yum install php8.1 -y
+yum install -y amazon-linux-extras
+amazon-linux-extras enable php8.1
 yum clean metadata
 yum install php php-{common,curl,mbstring,gd,gettext,bcmath,json,xml,fpm,intl,zip,imap}
 yum install wget
